ccea0ecf758c0b9ae9166b361a42c4c90e2801e5
Janet Jiang <janetjiang1103@gmail.com>
2023-08-30 11:25:04 -0700

    Adding .env file and API key

--- server/server.js
+++ server/server.js
@@ -2,12 +2,34 @@
 
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 app.use(cors());
 
 //Set the port that you want the server to run on
 const PORT = process.env.PORT || 8080;
+
+//Fetch request to the weather API
+app.get("/weather",(req,res)=>{
+  const city=req.query.cityName;
+  const apiKey= process.env.API_KEY;
+  const params= new URLSearchParams({
+    q: req.query.cityName,
+    appid: process.env.API_KEY,
+    units: "imperial",
+  });
+  const url=`https://api.openweathermap.org/data/2.5/weather?${params}`
+  console.log(url);
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      res.send({data});
+    })
+    .catch((err)=>{
+      console.log(err);
+    });
+})
 
 //creates an endpoint for the route /api
 app.get('/api', (req, res) => {
